5d0bd5e7fd61855ea235ac2d9beadecabb065254
Pharusa <pharusa2006@hotmail.com>
2020-02-28 09:57:52 +0100

    finished album

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#search").keyup(function() {
+
+        var albums = $(".album");
+        $(albums).show();
+
+var searchTerm = $(this) . val().toLocaleLowerCase()
+
+albums.each(function(){
+    if($(this).text().toLocaleLowerCase().search(searchTerm) == -1) {
+        $(this).hide();
+    }
+});
+});
+});
+
